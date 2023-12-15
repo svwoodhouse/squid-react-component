bd73ec93fbc702976c18538950c0fcc056f3a54b
Sydnee <svwoodhouse@gmail.com>
2023-12-15 18:33:45 -0500

    Added Button component and story

---
+++ src/components/Button/Button.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+export default {
+    title: 'Button',
+    component: Button
+}
+
+export const primary = () => <Button primary>Primary</Button>
+export const secondary = () => <Button secondary>Secondary</Button>
+export const success = () => <Button success>Success!</Button>
+export const danger = () => <Button danger>Danger!</Button>
+export const warning = () => <Button warning>Warning!</Button>
+export const outline = () => <Button outline>Outline</Button>
+export const rounded = () => <Button rounded>Rounded</Button>
